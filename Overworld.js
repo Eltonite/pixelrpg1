9c6cae2ea1c503c3923744dd84a7399bf6563245
Eltonite <eltoniteonblockchain@gmail.com>
2022-01-07 19:34:05 -0600

    Complete LB-RPG tutorial part 2

--- Overworld.js
+++ Overworld.js
@@ -14,45 +14,23 @@ class Overworld {
     image.src = "/images/maps/DemoLower.png"
 
 
+    //game object generations:
+    const hero = new GameObject({
+      x: 5,
+      y: 6,
+    })
 
-    const x = 1;
-    const y = 4;
-    const hero = new Image();
-    hero.onload = () => {
-      this.ctx.drawImage(
-        hero, 
-        0, 
-        0,
-        32,
-        32,
-        x*16 - 8,
-        y*16 - 20,
-        32,
-        32
-        )
-    }
-    hero.src = "/images/characters/people/hero.png"
+    const npc1 = new GameObject({
+      x: 7,
+      y: 9,
+      src: "/images/characters/people/npc1.png"
+    })
 
-
-
-    const shadow = new Image();
-    shadow.onload = () => {
-      this.ctx.drawImage(
-        shadow, 
-        0, 
-        0,
-        32,
-        32,
-        x*16 - 8,
-        y*16 - 20,
-        32,
-        32
-        )
-    }
-    shadow.src = "/images/characters/shadow.png"
-
-
-
+    setTimeout(() => {
+      hero.sprite.draw(this.ctx)
+      npc1.sprite.draw(this.ctx)
+    }, 200)
+    
 
   }
 }
---
+++ Sprite.js
@@ -0,0 +1,52 @@
+class Sprite {
+  constructor(config) {
+
+    //setup of images
+    this.image = new Image();
+    this.image.src = config.src;
+    this.image.onload = () => {
+      this.isLoaded = true;
+    }
+
+    //setup of shadow
+    this.shadow = new Image();
+    this.useShadow = true; //hardcoded now, will use config.useShadow later
+    if (this.useShadow) {
+      this.shadow.src = "/images/characters/shadow.png"
+    }
+    this.shadow.onload = () => {
+      this.isShadowLoaded = true;
+    }
+    
+
+    //Configs of animation and initial states
+    this.animations = config.animations || {
+      idleDown: [
+        [0,0]
+      ]
+    }
+    this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentAnimationFrame = 0;
+
+    //Reference the game object
+    this.gameObject = config.gameObject;
+  }
+
+  //draw the sprite function
+  draw(ctx) {
+    const x = this.gameObject.x * 16 - 7;
+    const y = this.gameObject.y * 16 - 18;
+
+    this.isShadowLoaded && ctx.drawImage(this.shadow, x, y)
+
+    this.isLoaded && ctx.drawImage(
+      this.image,
+      0,0,
+      32,32,
+      x,y,
+      32,32
+      )
+
+
+  }
+}
